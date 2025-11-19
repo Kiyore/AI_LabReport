4c5b372f69bf7fe75ad20655c16475020d67bf7e
YashChandaria <chandariayash@gmail.com>
2025-11-19 15:54:09 +0530

    Add BFS function for state exploration
    Implement breadth-first search (BFS) algorithm for pathfinding.

--- Code1.py
+++ Code1.py
@@ -1,1 +1,15 @@
+from collections import deque
 
+def bfs(startstate, goalstate, get_successors):
+    queue = deque([(startstate, [startstate])])
+    visited = set()
+    while queue:
+        currentstate, path = queue.popleft()
+        if currentstate == goalstate:
+            return path
+        if currentstate not in visited:
+            visited.add(currentstate)
+            for successor in get_successors(currentstate):
+                if successor not in visited:
+                    queue.append((successor, path + [successor]))
+    return "No solution found"
