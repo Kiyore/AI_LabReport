f1e247f4acb51546bf1794d7cc355b6635170778
YashChandaria <chandariayash@gmail.com>
2025-11-19 15:54:49 +0530

    Add DFS function for pathfinding
    Implement depth-first search algorithm.

--- Code2.py
+++ Code2.py
@@ -1,1 +1,13 @@
-
+ def dfs(startstate, goalstate, get_successors):
+    stack = [(startstate, [startstate])]
+    visited = set()
+    while stack:
+        currentstate, path = stack.pop()
+        if currentstate == goalstate:
+            return path
+        if currentstate not in visited:
+            visited.add(currentstate)
+            for successor in get_successors(currentstate):
+                if successor not in visited:
+                    stack.append((successor, path + [successor]))
+    return "No solution found"
